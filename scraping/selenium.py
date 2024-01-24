0e4b516fe9339fb20ccdba1e964a06bf287c8640
Younes Zerouali <younes_zerouali@hotmail.com>
2024-01-25 00:01:19 +0100

    Added headless server option - for deployment

--- scraping/selenium.py
+++ scraping/selenium.py
@@ -4,7 +4,12 @@ from selenium.webdriver.chrome.service import Service
 
 
 def init_chrome_driver():
+    # Set up Chrome options for headless mode
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')  # Run Chrome in headless mode
+    chrome_options.add_argument('--disable-gpu')  # Disable GPU acceleration
+
     service= Service( ChromeDriverManager().install() )
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
