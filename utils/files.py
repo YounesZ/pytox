8e74ee3b7661ff64ecc048fddfd6a7f790469125
Younes Zerouali <younes_zerouali@hotmail.com>
2025-06-02 23:20:36 +0100

    Removed duplicate function

--- utils/files.py
+++ utils/files.py
@@ -115,35 +115,3 @@ def remove_file(file_path: str) -> None:
         print(f"An error occurred while deleting file '{file_path}': {str(e)}")
     return done
 
-
-def list_files_with_date(min_date: datetime,
-                         max_date: datetime,
-                         region: Optional[str] = None,
-                         scraper: Optional[str] = None,
-                         folder: str = ''):
-
-    # List all files in data folder
-    ls_files = list_files_with_extension(folder, '.csv')
-
-    # Split file name
-    ls_regions = [i_.split('_')[0] if i_.count('_')==3 else 'other' for i_ in ls_files]
-    ls_scrapers = [i_.split('_')[1] if i_.count('_') == 3 else 'other' for i_ in ls_files]
-    ls_dates = [i_.split('_')[2] if i_.count('_')==3 else '21000101' for i_ in ls_files]
-
-    # --- Filter with dates
-    # Format dates
-    ls_dates = [datetime(year=int(i_[:4]), month=int(i_[4:6]), day=int(i_[6:8])) for i_ in ls_dates]
-
-    # Filter dates
-    fltr_files = [(i_<=max_date) & (i_>=min_date) for i_ in ls_dates]
-
-    # --- Filter with region
-    if not region is None:
-        fltr_files = [j_ & (i_ == region) for i_, j_ in zip(ls_regions, fltr_files)]
-
-    if not scraper is None:
-        fltr_files = [j_ & (i_ == scraper) for i_, j_ in zip(ls_scrapers, fltr_files)]
-
-    ok_files = [i_ for i_, j_ in zip(ls_files, fltr_files) if j_]
-
-    return ok_files
